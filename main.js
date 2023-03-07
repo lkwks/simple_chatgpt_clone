2ccbd4c97b79c930bce98741790a74973155c91c
Kangwook Lee <lkwks@naver.com>
2023-03-08 03:07:38 +0900

    Update main.js

--- main.js
+++ main.js
@@ -86,7 +86,7 @@ class ResponseDiv{
             let result = "";
             try{
                 const message = [{role: "assistant", content: chatgpt_api.messages[chatgpt_api.messages.length-1].content},
-                                 {role: "user", content: "What are the languages of these codes? Write your answer only in a JSON array. If you can't find it, just put \"plaintext\" at each code in the JSON array."}];
+                                 {role: "user", content: "What is the language(s) of this code? Write your answer only in a JSON array. If you can't find it, just put \"plaintext\" at each code in the JSON array. Only answer a JSON array, do not use code block."}];
                 const outputJson = await chatgpt_api.api(message);
                 console.log(outputJson.choices[0].message.content, processed);
                 
