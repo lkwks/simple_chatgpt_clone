c7442a316bd99dd10718ba03b854fac949abde19
Kangwook Lee <lkwks@naver.com>
2023-03-09 10:25:14 +0900

    Update main.js

--- main.js
+++ main.js
@@ -16,9 +16,12 @@ document.getElementById("prompt").select();
 class ChatGPTAPI{
     constructor()
     {
-        const system_message = localStorage.getItem("system_message");
-        this.system_message = system_message ? system_message : "";
-        this.messages = [{role:"system", content:this.system_message}];
+        this.messages = [{role:"system", content:""}];
+    }
+
+    set_system_message(prompt)
+    {
+        this.messages[0].content = prompt;
     }
 
     async api(messages)
@@ -36,7 +39,7 @@ class ChatGPTAPI{
 
     flush_messages()
     {
-        const cutIndex = Math.ceil(this.messages.length / 3) * 2;
+        const cutIndex = 2;
         this.messages = [{role:"system", content:this.system_message}, ...this.messages.slice(cutIndex, this.messages.length)];
     }
     
@@ -45,9 +48,8 @@ class ChatGPTAPI{
         this.messages.forEach( (e, i) => {
             if (e.content === elem.getAttribute("original_content"))
             {
-                this.messages = this.messages.splice(i, 1);
+                this.messages.splice(i, 1);
                 response_div.$target.removeChild(elem);
-                console.log(this.messages);
                 return;
             }
         });
@@ -56,11 +58,27 @@ class ChatGPTAPI{
     send(prompt)
     {
         if (!API_KEY) return false;
+
+        if (prompt.includes("System::"))
+        {
+            const system_message = prompt.replace("System::", "");
+            this.set_system_message(system_message);
+            return new Promise(resolve => resolve()).then(()=>{
+                const new_prompt = document.createElement("div");
+                new_prompt.classList.add("response_prompt");
+                new_prompt.setAttribute("original_content", system_message);
+                response_div.$target.appendChild(new_prompt);
+                new_prompt.innerHTML = `<pre>System message changed: ${system_message}</pre>`;
+                return false;
+            });
+        }
+        
+
         this.messages.push({ role: "user", content: prompt });
         
         return this.api(this.messages).then(outputJson => {
             this.messages.push({role: "assistant", content: outputJson.choices[0].message.content});
-            if (outputJson.usage.total_tokens > 3000) this.flush_messages();
+            if (outputJson.usage.total_tokens >= 4096) this.flush_messages();
             console.log(outputJson.usage.total_tokens);
             return true;
           }).catch(()=>{this.messages.pop(); return false;});
@@ -84,29 +102,32 @@ class ResponseDiv{
         else
         {
             let result = "";
-            try{
-                const message = [{role: "assistant", content: chatgpt_api.messages[chatgpt_api.messages.length-1].content},
-                                 {role: "user", content: "What is the language(s) of this code? Write your answer only in a JSON array, each language to each code block. If you can't find it, just put \"plaintext\" at each code in the JSON array. Note that your answer should not contain ``` in any situations."}];
-                const outputJson = await chatgpt_api.api(message);
-                console.log(outputJson.choices[0].message.content, processed);
-                
-                const languages = JSON.parse(outputJson.choices[0].message.content);
-                let splitted = processed.split("```");
-                for (var i=0; i < splitted.length - 1; i+=2)
-                {
-                    let code_content = splitted[i+1].split("\n");
-                    code_content[0] = "";
-                    if (languages[parseInt(i/2)] !== "LaTeX")
-                        result += `${splitted[i]}<code class="language-${languages[parseInt(i/2)]}">${code_content.join("\n")}</code>`;
-                    else
-                        result += `${splitted[i]}$$\n${code_content.join("\n")}\n$$`;
-                }
-                if (i % 2) result += splitted[splitted.length-1];
-            }
-            catch(e)
+            let splitted = processed.split("```");
+            for (var i=0; i < splitted.length - 1; i+=2)
             {
-                result = processed;
+                let code_content = splitted[i+1].split("\n");
+                let language = code_content[0].trim();
+                code_content[0] = "";
+                if (language === "LaTeX")
+                    result += `${splitted[i]}$$\n${code_content.join("\n")}\n$$`;
+                else if (language === "")
+                {
+                    try{
+                        const message = [{role: "assistant", content: code_content.join("\n")},
+                                         {role: "user", content: "What is the language of this code? Answer only in one word. Or, just answer this word: plaintext."}];
+                        const outputJson = await chatgpt_api.api(message);
+                        language = outputJson.choices[0].message.content.trim().replace(".", ""); 
+                        console.log(language);
+                    }
+                    catch(e)
+                    {
+                        language = "plaintext";
+                    }
+                }
+                result += `${splitted[i]}<code class="language-${language}">${code_content.join("\n")}</code>`;
             }
+            if (i % 2) result += splitted[splitted.length-1];
+
             return result;
         }
     }
@@ -114,23 +135,31 @@ class ResponseDiv{
     async update()
     {
         const new_prompt = document.createElement("div");
-        const new_response = document.createElement("div");
+    
+          new_prompt.classList.add("response_prompt");
+          new_prompt.setAttribute("original_content", chatgpt_api.messages[chatgpt_api.messages.length-2].content);
+          if (chatgpt_api.messages[chatgpt_api.messages.length-2].content !== "continue") 
+          {
+              this.$target.appendChild(new_prompt);
+              new_prompt.innerHTML = `<pre class="tex2jax_process">${await this.preprocess(chatgpt_api.messages[chatgpt_api.messages.length-2].content)}</pre><p>x</p>`;
+          }
 
-        new_prompt.classList.add("response_prompt");
-        new_response.classList.add("response_response");
-        
-        new_prompt.setAttribute("original_content", chatgpt_api.messages[chatgpt_api.messages.length-2].content);
-        new_response.setAttribute("original_content", chatgpt_api.messages[chatgpt_api.messages.length-1].content);
+          const new_response = document.createElement("div");
 
-        new_prompt.innerHTML = `<pre>${await this.preprocess(chatgpt_api.messages[chatgpt_api.messages.length-2].content)}</pre><p>x</p>`;
-        new_response.innerHTML = `<pre>${await this.preprocess(chatgpt_api.messages[chatgpt_api.messages.length-1].content)}</pre><p>x</p>`;
+          new_response.classList.add("response_response");
+          new_response.setAttribute("original_content", chatgpt_api.messages[chatgpt_api.messages.length-1].content);
+          this.$target.appendChild(new_response);
+          new_response.innerHTML = `<pre class="tex2jax_process">${await this.preprocess(chatgpt_api.messages[chatgpt_api.messages.length-1].content)}</pre><p>x</p>`;
+          hljs.highlightAll();
 
-        if (chatgpt_api.messages[chatgpt_api.messages.length-2].content !== "continue") this.$target.appendChild(new_prompt);
-        this.$target.appendChild(new_response);
-        hljs.highlightAll();
-        MathJax.typeset();
+          await MathJax.typesetPromise().then(async () => {
+            MathJax.typesetPromise();
+        }).catch((err) => console.log(err.message));
+
     }
 }
+
+
 
 const response_div = new ResponseDiv(document.querySelector("div.response"));
 
@@ -153,19 +182,8 @@ document.querySelector("div.prompt > input").addEventListener("click", ()=>{
             document.querySelector("div.prompt > textarea").readOnly = false;
             document.querySelector("div.prompt > textarea").classList.remove("readOnly");
             document.getElementById("prompt").select();
-            document.querySelectorAll("div.response > *")[document.querySelectorAll("div.response > *").length - 2].scrollIntoView({ behavior: 'smooth' });
+            if (document.querySelectorAll("div.response > *").length > 1)
+                document.querySelectorAll("div.response > *")[document.querySelectorAll("div.response > *").length - 2].scrollIntoView({ behavior: 'smooth' });
         });
     }
 });
-
-
-    window.onload = function() {
-        MathJax.Hub.Config({
-            tex2jax: {inlineMath: [['$','$']], displayMath: [['$$','$$']]},
-            extensions: ['tex2jax.js'],
-            jax: ['input/TeX', 'output/HTML-CSS'],
-            showProcessingMessages: false,
-            messageStyle: 'none'
-        });
-        MathJax.Hub.Queue(["Typeset",MathJax.Hub]);
-    }
