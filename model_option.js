93e547a4040a2d635b83ae15ce88ba3bed294a04
Kangwook Lee <lkwks@naver.com>
2023-12-28 18:27:29 +0900

    Update model_option.js to use gpt-3.5-turbo-1106 as default

--- model_option.js
+++ model_option.js
@@ -3,7 +3,7 @@ export class ModelOption {
         this.$target = $target;
         this.model = "";
         this.token = 0;
-        this.select("gpt-3.5-turbo", 4096);
+        this.select("gpt-3.5-turbo-1106", 4096);
     }
 
     select(model, token) {
