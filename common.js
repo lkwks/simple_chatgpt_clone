835985b5fd8cf4bf9e208584e2877ca0f4b52d5f
Kangwook Lee <lkwks@naver.com>
2023-12-29 14:03:36 +0900

    Update common.js

--- common.js
+++ common.js
@@ -73,12 +73,20 @@ function post_process(DOMelem, message, system_message="") {
     var html = document.createElement("p");
     html.innerHTML = markdown_converter.makeHtml(message);
 
+    // html(answer_stream.answer_set을 마크다운 포매팅)의 자식 엘리먼트가 둘 이상인 경우.
     if (html.childElementCount > 1) {
+        // DOMelem의 마지막 자식은 텍스트 스트림이 일어나고 있었던 엘리먼트.
+        // html의 자식 엘리먼트가 둘 이상이란 이야기는, DOMelem의 기존 마지막 자식에 관한 스트림이 끝났단 이야기. 기존 미완성품 지워줘야.
+        DOMelem.removeChild(DOMelem.lastChild);
+
         let remain = answer_stream.answer_set.replace(html.lastChild.textContent, "");
         console.log("a", answer_stream.answer_set);
-        console.log("b", html.lastChild.textContent);
+        console.log("b", html.lastChild.outerHTML);
+
+        // answer_stream.answer_set에서는 이미 엘리먼트가 다 완성된 부분의 텍스트를 지워준다.
+        // 이로써 answer_stream.answer_set에는 DOMelem의 마지막 자식 내 텍스트 스트림을 위한 텍스트만 남는다.
         answer_stream.answer_set = answer_stream.answer_set.replace(remain, "");
-        DOMelem.removeChild(DOMelem.lastChild);
+
         html.childNodes.forEach(el => {
             el.classList.add("tex2jax_process");
             DOMelem.appendChild(el);
