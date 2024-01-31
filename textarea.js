8bc93e9f3cdbe73eaf25c33abc90db110449acd3
Kangwook Lee <lkwks@naver.com>
2024-01-31 21:46:03 +0900

    Update textarea.js

--- textarea.js
+++ textarea.js
@@ -140,7 +140,7 @@ export class Textarea{
         this.$target.value = "Generating...";
         messages.scrollIntoView();
 
-        await chatgpt_api([messages.system_message, ...messages.messages], true).then(async response => {
+        await chatgpt_api([/* messages.system_message,*/ ...messages.messages], true).then(async response => {
             const reader = response.body.getReader();
             let buffer = '';
 
